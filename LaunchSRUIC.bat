8f08bd74e2286f1d7df9916b860a3486cd7c49f4
Richard Sayer <rasayer@gmail.com>
2025-02-06 07:13:45 +0000

    added snowrunner path as a variable

--- LaunchSRUIC.bat
+++ LaunchSRUIC.bat
@@ -1,4 +1,8 @@
-
-start /d "C:\Program Files (x86)\Steam\steamapps\common\SnowRunner\Sources\Bin" SnowRunner.exe
+:: set the path to snowrunner
+set snowrunnerPath=C:\Program Files (x86)\Steam\steamapps\common\SnowRunner\Sources\Bin
+:: launch snowrunner
+start /d "%snowrunnerPath%" SnowRunner.exe
+:: wait 5 seconds
 timeout 5 > NUL
-start /d "C:\Program Files (x86)\Steam\steamapps\common\SnowRunner\Sources\Bin" SRUICInjector.exe"
+:: launch SRUIC
+start /d "%snowrunnerPath%" SRUICInjector.exe
