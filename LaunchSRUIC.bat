5499beb3c4ee463103352e9cd7b07873896dda7a
Richard Sayer <rasayer@gmail.com>
2025-02-06 06:43:54 +0000

    Changed to simple batch file
    changed to a simple batch file

---
+++ LaunchSRUIC.bat
@@ -0,0 +1,4 @@
+
+start /d "C:\Program Files (x86)\Steam\steamapps\common\SnowRunner\Sources\Bin" SnowRunner.exe
+timeout 5 > NUL
+start /d "C:\Program Files (x86)\Steam\steamapps\common\SnowRunner\Sources\Bin" SRUICInjector.exe"
